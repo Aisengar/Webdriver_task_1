5d359d62bea783bab295eff1be47fb895da1c04c
Camilo Delgado <camilod@createme.com>
2024-05-20 14:57:28 -0500

    Working on task 2, only missing the posted information check

--- src/po/data/testdata.js
+++ src/po/data/testdata.js
@@ -1,7 +1,13 @@
 const testData = {
     codeText: "Hello from WebDriver",
     Title: "helloweb",
-    expirationText: "10 Minutes"
+    expirationText: "10 Minutes",
+    syntaxText: "Bash",
+    codeText2: `git config --global user.name'New Sheriff in Town'
+    git reset $(git commit-tree HEAD^{tree} -m 'Legacy code')
+    git push origin master --force
+    git push heroku master`,
+    Title2: "how to gain dominance among developers"
 };
 
 module.exports = testData;
--- src/test/specs/pastebin.test.js
+++ src/test/specs/pastebin.test.js
@@ -1,5 +1,4 @@
-//const { Key } = require('webdriverio');
-
+const Dropdown = require('../../po/components/dropdown.component.js');
 const testData = require('../../po/data/testdata.js');
 const PastebinPage = require('../../po/pages/pastebin.page.js');
 
@@ -13,51 +12,65 @@ describe("WebdriverIO Test", () => {
         await browser.reloadSession();
     });
 
-    describe("Create a new Paste", () => {
+    describe("Task 1: Create a paste and verify it", () => {
         it("Create a new paste with the following parameters: Title: helloweb, Code: Hello from WebDriver, Expiration: 10 Minutes", async () => {  
             const pastebinPage = new PastebinPage(); //Initialize the PastebinPage object  
+            
             // Input the code for the paste
             await pastebinPage.setCode(testData.codeText);
+            
             //imput the title for the paste
             await pastebinPage.setTitle(testData.Title); 
+            
             // Set the expiration to 10 minutes
             await pastebinPage.setExpiration(testData.expirationText);
-
+            
             // Verify the code input
             const actualCode = await pastebinPage.codeInput.getValue();
             expect(actualCode).toEqual(testData.codeText);
-    
+            
             // Verify the title input
             const actualTitle = await pastebinPage.titleInput.getValue();
             expect(actualTitle).toEqual(testData.Title);
-    
+            
             // Verify the expiration selection
             const selectedOption = await pastebinPage.expirationDropdown.selectedText();
-            console.log(selectedOption);
             expect(selectedOption).toEqual(testData.expirationText);
-    
-            // Click on the "Create New Paste" and wait for 2 seconds so we can check if the paste data is okay
-            await browser.pause(2000);
+            
+            // Click on the "Create New Paste" button
             await pastebinPage.createPaste();
-    
+            
             // Verification to ensure the paste was created
             await browser.pause(5000);
             const newPasteUrl = await pastebinPage.getCurrentUrl();
             console.log(`New Paste URL: ${newPasteUrl}`); // Log the URL of the new paste
         });
-        /*
-        // Select syntax highlighting and verify it
-        it("Select syntax highlighting", async () => {
-            const syntaxDropdown = await browser.$('#select2-postform-format-container');
-            await syntaxDropdown.click();
-            const syntaxSearchInput = await browser.$('.select2-search__field');
-            await syntaxSearchInput.setValue('Bash');
-            await syntaxSearchInput.addValue(Key.Enter);
-
-            //Testea que se a selecciono el elemento deseado Bash
-            const selectedSyntax = await syntaxDropdown.getText();
-            console.log(selectedSyntax);
-            expect(selectedSyntax).toEqual("Bash");
-        });*/
     });
+
+    describe("Task 2: create a new paste adding diferent values", () => {
+        it("Create a new paste with the following parameters: Title: how to gain dominance among developers, Code: git , Expiration: 10 Minutes, syntax HL: Bash", async () => {
+            const pastebinPage = new PastebinPage(); //Initialize the PastebinPage object
+            
+            // Input the code for the paste
+            await pastebinPage.setCode(testData.codeText2);
+            
+            //imput the title for the paste
+            await pastebinPage.setTitle(testData.Title2); 
+            
+            // Set the expiration to 10 minutes
+            await pastebinPage.setExpiration(testData.expirationText);
+
+            // Select syntax highlighting and verify it
+            const syntaxDropdown = new Dropdown('#select2-postform-format-container', '.select2-search__field');
+            await syntaxDropdown.SelectByVisibleText('Bash');
+            
+            // Click on the "Create New Paste" button
+            await pastebinPage.createPaste();
+
+            // Verification to ensure the paste was created
+            await browser.pause(8000);
+            const newPasteUrl = await pastebinPage.getCurrentUrl();
+            console.log(`New Paste URL: ${newPasteUrl}`); // Log the URL of the new paste
+        })
+    })    
 });

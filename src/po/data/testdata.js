fdba11c2675e28f700f0458f2fa715089628656c
Camilo Delgado <camilod@createme.com>
2024-05-20 19:46:35 -0500

    Fixing codeText2 Identation

--- src/po/data/testdata.js
+++ src/po/data/testdata.js
@@ -3,10 +3,7 @@ const testData = {
     Title: "helloweb",
     expirationText: "10 Minutes",
     syntaxText: "Bash",
-    codeText2: `git config --global user.name'New Sheriff in Town'
-    git reset $(git commit-tree HEAD^{tree} -m 'Legacy code')
-    git push origin master --force
-    git push heroku master`,
+    codeText2: "git config --global user.name'New Sheriff in Town'/ngit reset $(git commit-tree HEAD^{tree} -m 'Legacy code')\ngit push origin master --force\ngit push heroku master",
     Title2: "how to gain dominance among developers",
     codetest: "git reset $(git commit-tree HEAD^{tree} -m 'Legacy code')"
 };
